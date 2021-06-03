23a0de4f8df298ee1af6d7d3891301b80bb059fc
Dimas0075 <nugrohodimas59@gmail.com>
2021-06-03 15:48:54 +0700

    Menambah bagian Home dan Detail Imunisasi

--- app/src/main/java/its/nugrohodimas/capstonebangkit/ui/home/HomeFragment.kt
+++ app/src/main/java/its/nugrohodimas/capstonebangkit/ui/home/HomeFragment.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import its.nugrohodimas.capstonebangkit.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import its.nugrohodimas.capstonebangkit.databinding.FragmentHomeBinding
+import its.nugrohodimas.core.domain.model.Article
+import its.nugrohodimas.core.domain.model.Vaccine
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
+    private val dataList: MutableList<Article> = mutableListOf()
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,13 +30,14 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding.rvHomeInformation.layoutManager = GridLayoutManager(context, 2)
+        val db = FirebaseFirestore.getInstance()
+        db.collection("article").get()
+            .addOnSuccessListener {
+
+            }
+        return binding.root
     }
 
     override fun onDestroyView() {
